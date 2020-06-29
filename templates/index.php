b744395259f6c8cad5c88312b6fc5a32e9be9b8e
kkbrains <kishorekumaru@gmail.com>
2020-06-29 18:49:11 +1000

    Reverted to base

--- templates/index.php
+++ templates/index.php
@@ -1,9 +1,18 @@
 <?php
-script('pdftron', 'script');
-style('pdftron', 'style');
+script('smartxchangepdftron', 'script');
+style('smartxchangepdftron', 'style');
 ?>
 
 <div id="app">
-	<?php print_unescaped($this->inc('content/index')); ?>
+	<div id="app-navigation">
+		<?php print_unescaped($this->inc('navigation/index')); ?>
+		<?php print_unescaped($this->inc('settings/index')); ?>
+	</div>
+
+	<div id="app-content">
+		<div id="app-content-wrapper">
+			<?php print_unescaped($this->inc('content/index')); ?>
+		</div>
+	</div>
 </div>
 
